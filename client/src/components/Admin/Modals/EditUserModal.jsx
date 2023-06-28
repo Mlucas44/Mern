6022244c4dbede6efe81fdf0f651aad6b272979b
lmetayer <lmetayer@la-joliverie.com>
2023-06-28 15:08:49 +0200

    update admin

--- client/src/components/Admin/Modals/EditUserModal.jsx
+++ client/src/components/Admin/Modals/EditUserModal.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-const EditUserModal = ({ show, handleClose, updatedUser, handleChange, handleSubmit }) => {
+const EditUserModal = ({ show, handleClose, updatedUser, handleEditChange, handleEditSubmit }) => {
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Modifier l'utilisateur {updatedUser.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleEditSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input value={updatedUser.name || ''} onChange={handleChange} type="text" className="form-control" id="name" placeholder="Name" name="name" />
+                        <input value={updatedUser.name || ''} onChange={handleEditChange} type="text" className="form-control" id="name" placeholder="Name" name="name" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="username" className="form-label">Username</label>
-                        <input value={updatedUser.username || ''} onChange={handleChange} type="text" className="form-control" id="username" placeholder="Username" name="username" />
+                        <input value={updatedUser.username || ''} onChange={handleEditChange} type="text" className="form-control" id="username" placeholder="Username" name="username" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input value={updatedUser.email || ''} onChange={handleChange} type="email" className="form-control" id="email" placeholder="Email" name="email" />
+                        <input value={updatedUser.email || ''} onChange={handleEditChange} type="email" className="form-control" id="email" placeholder="Email" name="email" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input value={updatedUser.password || ''} onChange={handleChange} type="password" className="form-control" id="password" placeholder="Password" name="password" />
+                        <input value={updatedUser.password || ''} onChange={handleEditChange} type="password" className="form-control" id="password" placeholder="Password" name="password" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="role" className="form-label">Role</label>
-                        <select value={updatedUser.role} onChange={handleChange} className="form-select" id="role" name="role">
-                        <option value="" disabled defaultValue>Select role...</option>
-                        <option value="user">User</option>
-                        <option value="admin">Admin</option>
+                        <select value={updatedUser.role} onChange={handleEditChange} className="form-select" id="role" name="role">
+                            <option value="" disabled defaultValue>Select role...</option>
+                            <option value="user">User</option>
+                            <option value="admin">Admin</option>
                         </select>
                     </div>
                 </form>
@@ -39,7 +39,7 @@ const EditUserModal = ({ show, handleClose, updatedUser, handleChange, handleSub
                 <Button variant="secondary" onClick={handleClose}>
                     Annuler
                 </Button>
-                <Button variant="primary" onClick={handleSubmit}>
+                <Button variant="primary" onClick={handleEditSubmit}>
                     Enregistrer
                 </Button>
             </Modal.Footer>
