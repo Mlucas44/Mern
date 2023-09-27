8e94daf472bf614573047fc3243f13a46fa08b53
lmetayer44 <144024011+lmetayer44@users.noreply.github.com>
2023-09-27 16:57:26 +0200

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,38 +4,42 @@ import { NavLink } from 'react-router-dom'
 import './navbar.scss'
 import { useLogout } from './../../hooks/useLogout'
 import useAuthContext from './../../hooks/useAuthContext'
+import logo from './logo.png';
 
 const Navbar = ({ userInfo }) => {
-
   return (
-    <>
-      <div className="nav-container">
-        <nav className="navbar">
-          <div className="nav-flex">
-            <div className="logo">
-              <div className='logo-item'>
-                logo
-              </div>
-              <h1>Mern_Profile</h1>
-            </div>
-            <div className="primary-menu">
-              <ul className="menu">
-                <li>
-                  <NavLink to="/" className='menu-item'>Home</NavLink>
-                </li>
-                {userInfo && userInfo.role === 'admin' && (
-                  <li>
-                    <NavLink to="/admin" className='menu-item'>Admin</NavLink>
-                  </li>
-                )}
-              </ul>
-            </div>
+      <>
+          <div className="nav-container">
+              <nav className="navbar">
+                  <div className="logo">
+                      <img src={logo} alt="Logo" className="navbar-logo"></img>
+                      <h1>Club Football</h1>
+                  </div>
+                  <div className="primary-menu">
+                      <ul className="menu">
+                          <li>
+                              <NavLink to="/" className='menu-item'>Accueil</NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/club" className='menu-item'>Club</NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/equipes" className='menu-item'>Equipes</NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/partenaire" className='menu-item'>Partenaire</NavLink>
+                          </li>
+                          {userInfo && userInfo.role === 'admin' && (
+                              <li>
+                                  <NavLink to="/admin" className='menu-item'>Admin</NavLink>
+                              </li>
+                          )}
+                      </ul>
+                  </div>
+                  {userInfo ? (<DropdownMenu userInfo={userInfo} />) : (<LoginMenu />)}
+              </nav>
           </div>
-          {userInfo && (<DropdownMenu userInfo={userInfo} />)}
-          {!userInfo && (<LoginMenu />)}
-        </nav>
-      </div>
-    </>
+      </>
   )
 }
 
@@ -67,11 +71,6 @@ const DropdownMenu = ({ userInfo }) => {
             <li>
               <NavLink to="/profile">Mon profil</NavLink>
             </li>
-            {userInfo && userInfo.role === 'admin' && (
-              <li>
-                <NavLink to="/admin" className='menu-item'>Admin</NavLink>
-              </li>
-            )}
             <li>
               <NavLink onClick={handleclick} className='btn btn-dark btn-icon'>
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 13h1a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-1M6 11l3-3-3-3M8.5 8H3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" vectorEffect="non-scaling-stroke"></path></svg>
